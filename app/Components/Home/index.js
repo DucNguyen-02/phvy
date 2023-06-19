4ac62d990bf91076a64d995def866e75d7d4cc71
Xuan Duc <nguyenxuanduc0123@gmail.com>
2023-06-19 21:01:59 +0700

    a

--- app/Components/Home/index.js
+++ app/Components/Home/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div className="grid grid-cols-2">
+      <img src="/images/pana.jpg" className="w-[100%] h-full m-auto" />
+      <img src="/images/maygiat.jpg" className="w-[100%] h-full m-auto" />
+      <img src="/images/a.jpg" className="w-[100%] h-full m-auto" />
+      <img src="/images/tivi.jpg" className="w-[100%] h-full m-auto" />
+    </div>
+  )
 }
 
 export default Home
